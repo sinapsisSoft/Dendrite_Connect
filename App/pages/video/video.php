38c7a3dc4082c49aedb3eeacfbff8f474f622dc1
lauramgrisalesgarcia <41131779+lauramgrisalesgarcia@users.noreply.github.com>
2020-10-07 20:32:18 -0500

    Template update

--- App/pages/video/video.php
+++ App/pages/video/video.php
@@ -48,7 +48,7 @@ if (!isset($_SESSION['User'])) {
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Revisa los videos </h1>
+            <h1 class="h3 mb-0 text-gray-800">La trivia del pulmón </h1>
             <!--<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>-->
           </div>
           <!-- Content Row -->
